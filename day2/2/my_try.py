e36cb4177c87eaf1b4e841fd2ebbe9c3a4dcc360
Jacopo Sodini <jsodini@develer.com>
2021-03-10 13:39:00 +0100

    Aggiunta feature per creare repo XML nell'esercizio 2 day 2

--- day2/2/my_try.py
+++ day2/2/my_try.py
@@ -29,6 +29,45 @@ def test_site(URLS):
             
     return tested
 
+       
+def make_XML(sites,destination):
+    '''
+<urltests>
+  <test>
+    <url>http://www.python.org</url>
+    <result>fail</result>
+    <details>
+      <connectiontest>success</connectiontest>
+      <timetest elapsed="2410" maxtime="2000">fail</timetest>
+    </details>
+  </test>
+  ...
+</urltests>
+'''
+    troot = ET.Element('urltests')
+    try:
+        with open(destination, "wb") as f:
+        
+            for url,rx_time,status,online,max_time in sites:
+                ttest = ET.SubElement(troot, 'test')
+                turl = ET.SubElement(ttest, 'url')
+                turl.text = url            
+                tdetails = ET.SubElement(ttest,'details')
+                tconn = ET.SubElement(tdetails, 'connectiontest')
+                tconn.text = 'success' if online else 'fail'            
+            
+                if online:
+                    #ttimetest = ET.SubElement(tdetails, 'timetest', {'elapsed': '{:.2f}'.format(t), 'maxtime': '{:.2f}'.format(max_time)})
+                    ttimetest = ET.SubElement(tdetails, 'timetest', {'elapsed': '{:.2f}'.format(rx_time*1000), 'maxtime': '{:.2f}'.format(max_time)})
+                    #ttimetest = ET.SubElement(tdetails, 'timetest')
+                    ttimetest.text = 'success' if (rx_time*1000) < max_time else 'failed'
+                    
+            f.write(ET.tostring(troot))
+    except IOError:
+        print("Can't open XML file {}".format(destination))
+        
+    return troot
+        
 def main():
     
     parser=argparse.ArgumentParser(description='Test websites')
@@ -41,6 +80,9 @@ def main():
         response = "OK" if online else "KO"
         print ("{:40} {:.3f} {} \t\t{}".format(url,rx_time,status,response))
         
+    if args.x:
+        #print (args.x)
+        XML=make_XML(tested, args.x)  
 
 
 if __name__=='__main__':
