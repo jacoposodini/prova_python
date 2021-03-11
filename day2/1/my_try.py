42498b5102b26671b0bba20e423db34e30626765
Jacopo Sodini <jsodini@develer.com>
2021-03-11 12:07:51 +0100

    modificato 'if l is not none'

--- day2/1/my_try.py
+++ day2/1/my_try.py
@@ -12,12 +12,12 @@ REG = re.compile(
     
 def get_errors(path,err):
     
-    log=[]
+    log = []
     
     with open(path) as f:
         for line in f.readlines():
             l = REG.search(line)            
-            if l!= None and l.group('status_code') == err:
+            if l is not None and l.group('status_code') == err:
                 #print (l.group('date'), " ", l.group('referrer'), " ", l.group('resource'))
                 date = datetime.strptime(l.group('date'), '%d/%b/%Y:%H:%M:%S')
                 log.append((date,l.group('referrer'),l.group('resource')))
@@ -29,9 +29,9 @@ def main():
     
     parser.add_argument('path', help='log to be read')
     parser.add_argument('err_code', help='error code')
-    args=parser.parse_args()
+    args = parser.parse_args()
     
-    log=get_errors(args.path,args.err_code)
+    log = get_errors(args.path,args.err_code)
     
     for date, referrer, resource in log:
         print ("{date} {res} {ref}".format(date=date.strftime("%d/%m/%y %H:%M"), res=resource, ref=referrer))
