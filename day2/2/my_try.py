551904a69db46eaa09736938ce36a9664a8a9179
Jacopo Sodini <jsodini@develer.com>
2021-03-11 13:28:20 +0100

    modificata la gestione del TIMEOUT e OFFLINE

--- day2/2/my_try.py
+++ day2/2/my_try.py
@@ -3,6 +3,11 @@ import time
 import argparse
 import xml.etree.ElementTree as ET
 
+class ConnectionStatus:
+   ONLINE = "ONLINE"
+   TIMEOUT = "TIMEOUT"
+   OFFLINE = "OFFLINE"
+
 
 URLS = {
     'http://www.python.org': 1000,
@@ -16,16 +21,16 @@ URLS = {
 def test_site(URLS):
     tested=[]
     for i in URLS:
-        online = False
+        online = ConnectionStatus.OFFLINE
         try:            
             start_time=time.time()
             req = requests.request('GET', i, timeout=URLS[i]/1000)
-            online=True
-            tested.append([i,time.time()-start_time,req.status_code,online,URLS[i]])
+            online = ConnectionStatus.ONLINE
+            tested.append([i,time.time()-start_time,req.status_code,ConnectionStatus.ONLINE,URLS[i]])
         except requests.Timeout:
-            tested.append([i,time.time()-start_time,'Timeout',online,URLS[i]])
+            tested.append([i,time.time()-start_time,None,ConnectionStatus.TIMEOUT,URLS[i]])
         except requests.RequestException as e:
-            tested.append([i,time.time()-start_time,'ERR',online,URLS[i]])
+            tested.append([i,time.time()-start_time,None,ConnectionStatus.OFFLINE,URLS[i]])
             
     return tested
 
@@ -48,19 +53,17 @@ def make_XML(sites,destination):
     try:
         with open(destination, "wb") as f:
         
-            for url,rx_time,status,online,max_time in sites:
+            for url,rx_time,code,status,max_time in sites:
                 ttest = ET.SubElement(troot, 'test')
                 turl = ET.SubElement(ttest, 'url')
                 turl.text = url            
                 tdetails = ET.SubElement(ttest,'details')
                 tconn = ET.SubElement(tdetails, 'connectiontest')
-                tconn.text = 'success' if online else 'fail'            
+                tconn.text = 'success' if status == ConnectionStatus.ONLINE else 'fail'
             
-                if online:
-                    #ttimetest = ET.SubElement(tdetails, 'timetest', {'elapsed': '{:.2f}'.format(t), 'maxtime': '{:.2f}'.format(max_time)})
-                    ttimetest = ET.SubElement(tdetails, 'timetest', {'elapsed': '{:.2f}'.format(rx_time*1000), 'maxtime': '{:.2f}'.format(max_time)})
-                    #ttimetest = ET.SubElement(tdetails, 'timetest')
-                    ttimetest.text = 'success' if (rx_time*1000) < max_time else 'failed'
+                if status == ConnectionStatus.ONLINE:                    
+                    ttimetest = ET.SubElement(tdetails, 'timetest', {'elapsed': '{:.2f}'.format(rx_time*1000), 'maxtime': '{:.2f}'.format(max_time)})                    
+                    ttimetest.text = 'success' if status is not ConnectionStatus.TIMEOUT else 'failed'
                     
             f.write(ET.tostring(troot))
     except IOError:
@@ -70,19 +73,19 @@ def make_XML(sites,destination):
         
 def main():
     
-    parser=argparse.ArgumentParser(description='Test websites')
+    parser = argparse.ArgumentParser(description='Test websites')
     parser.add_argument('-x', help="Print a XML report")
     args = parser.parse_args()
     
        
-    tested=test_site(URLS)
-    for url,rx_time,status,online,max_time in tested:
-        response = "OK" if online else "KO"
+    tested = test_site(URLS)
+    for url,rx_time,code,status,max_time in tested:
+        response = "OK" if status == ConnectionStatus.ONLINE else "KO"
         print ("{:40} {:.3f} {} \t\t{}".format(url,rx_time,status,response))
         
     if args.x:
         #print (args.x)
-        XML=make_XML(tested, args.x)  
+        XML = make_XML(tested, args.x)  
 
 
 if __name__=='__main__':
