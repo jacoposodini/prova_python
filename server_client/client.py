570394c7a01de9073fc22fcb583348ea1feff117
Jacopo Sodini <jsodini@develer.com>
2021-03-12 14:26:43 +0100

    creato applicativo client - server

---
+++ server_client/client.py
@@ -0,0 +1,19 @@
+import socket
+
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 65432        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    
+    with s:
+        while True:
+            cmd = input('>')
+            cmd = str.encode(cmd)
+            s.sendall(cmd)
+            data = s.recv(1024)
+
+            if data == b"quit":
+                break
+            else:
+                print('######Received######\n', data.decode('utf-8'))
---
+++ server_client/server.py
@@ -0,0 +1,46 @@
+import socket
+import subprocess
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+commands = {b'list':b'ls', b'concat':b'cat', b'print':b'echo'}
+help_str = b"A simple server that map the following commands to the equivalent linux version:\n\
+            list --> ls\n\
+            concat --> cat\n\
+            print --> echo"
+
+#list_files = subprocess.run(["ls"], stdout=subprocess.PIPE)
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #per evitare il lock della porta
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    
+    print('Connected by', addr)
+    
+    with conn:
+        while True:        
+            
+            data = conn.recv(1024)
+            
+            if data != b'':
+            
+                cmd_split = data.split()
+                cmd = cmd_split[0]
+            
+                #conn.sendall(data) #echo
+            
+                if cmd in commands:
+                    command = [commands[cmd]]+cmd_split[1:]
+                    cmd_return_string = subprocess.run(command, stdout=subprocess.PIPE)      
+                    conn.sendall(cmd_return_string.stdout)
+                elif data == b"quit":
+                    conn.sendall(data)                
+                    break
+                else:
+                    conn.sendall(help_str)
+                    
+            
