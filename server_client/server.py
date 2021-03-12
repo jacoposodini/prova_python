0c8fa0efbfee39328aee2616dac5143c378d03d5
Jacopo Sodini <jsodini@develer.com>
2021-03-12 17:26:53 +0100

    sostituita lista nel dizionario con una tupla

--- server_client/server.py
+++ server_client/server.py
@@ -6,7 +6,7 @@ PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 #commands is a dict in this form: {SERVER_COMMAND:[LINUX_COMMAND,MIN_PARAM]}
 
-commands = {b'list':[b'ls',0], b'concat':[b'cat',1], b'print':[b'echo',0]}
+commands = {b'list':(b'ls',0), b'concat':(b'cat',1), b'print':(b'echo',0)}
 help_str = b"A simple server that map the following commands to the equivalent linux version:\n\
             list --> ls\n\
             concat --> cat\n\
